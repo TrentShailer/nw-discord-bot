aa8c13d8a15cc32300e68035c94c87c04b3485b7
Trent Shailer <trent.shailer@gmail.com>
2021-12-15 13:00:28 +1300

    fixed import typo

--- commands/elite.js
+++ commands/elite.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 let data = require("../data/elites.json");
-const { MessageEmbed } = require("discordjs");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
